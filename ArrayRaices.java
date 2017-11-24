3b0a9c6da5b82a7e8eb5d4ec542b39089cc56f62
maria <mgcmaria@gmail.com>
2017-11-24 12:57:25 +0100

    classpath

---
+++ ArrayRaices.java
@@ -0,0 +1,17 @@
+
+public class ArrayRaices {
+	
+	public static void main(String[] args)
+	{
+		int [] numero = new int [10];
+		double [] raiz = new double [10];
+		int i;
+		
+		for (i=0; i<numero.length; i++)
+				{
+					numero[i]=i+1;
+					raiz[i]=Math.sqrt(numero[i]);
+					System.out.println(numero[i] + " : " + raiz [i]);	
+				}
+}
+}
